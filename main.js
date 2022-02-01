36648027cecc36b666f96df95209a8634995fb5a
Oleksandr <sashakryvoruchko11@gmail.com>
2022-02-01 19:30:52 +0200

    main page

---
+++ main.js
@@ -0,0 +1,11 @@
+import './sass/style.scss';
+import './sass/blocks/header.scss';
+import './sass/blocks/footer.scss';
+import './sass/blocks/content.scss';
+import './sass/media.scss';
+
+
+// document.querySelector('#app').innerHTML = `
+//   <h1>Hello Vite!</h1>
+//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+// `
